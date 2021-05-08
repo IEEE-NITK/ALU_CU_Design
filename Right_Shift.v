5b4985d4cce9901f280c3258f55a13a608295e3d
harsh25112002 <77107243+harsh25112002@users.noreply.github.com>
2021-05-08 23:13:09 +0530

    Add files via upload

---
+++ Right_Shift.v
@@ -0,0 +1,64 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08.05.2021 23:01:00
+// Design Name: 
+// Module Name: Right_Shift
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08.05.2021 15:16:14
+// Design Name: 
+// Module Name: Left_Shift
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Right_Shift(input [31:0] A,C, output reg [31:0] B , output reg  Z ,N );
+
+always@(A or C)
+begin
+ B = A>>C;
+    if (B == 32'h00000000)
+        begin
+          Z = 1;
+          N = 0;
+        end
+      else
+        begin
+          Z = 0;
+          if (B[31] == 1'b0)
+            N = 0;
+          else
+            N = 1;
+        end
+end
+endmodule
---
+++ Right_Shift_Test.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08.05.2021 17:51:42
+// Design Name: 
+// Module Name: Left_Shift_Test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Right_Shift_Test;
+reg [31:0] A,C;
+wire [31:0] B;
+Right_Shift RS(A,C,B,Z,N);
+initial
+begin
+$monitor("     A      C     B     N      Z   ");
+
+
+assign  A=32'h00011000 ; C=32'h00000001;
+#(2)
+assign  A=-32'h10010100 ; C=32'h00000001;
+#(2)
+assign  A=32'h00100010 ; C=-32'h00000001;
+end
+    
+endmodule

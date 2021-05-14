91b3fabaa1af059bfc95da55d14acfb78ab8ad21
harsh25112002 <77107243+harsh25112002@users.noreply.github.com>
2021-05-15 01:23:46 +0530

    Update Alu_test_bench_orignial_code.v

--- Alu_test_bench_orignial_code.v
+++ Alu_test_bench_orignial_code.v
@@ -26,7 +26,7 @@ reg [5:0]opCode;
 reg c;
 wire [31:0] ans1;
 wire ans2,Z,N;
-ALU Alu(a, b, opCode,c, ans1, ans2, Z, N);
+ALU Alu(a, b, opCode, ans1, ans2, Z, N);
   FA_32bit FA ( a,b, c,ans1,ans2);
   FS_32bit FS(a,b, c,ans1,ans2);
   E_32bit E(a,b,ans1 );
